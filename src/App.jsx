8b2de3bc190640eb31c2b8ed54c190cab975fe72
darylwanji <daryl.wanji@gmail.com>
2025-01-31 19:24:27 -0500

    Update main heading text from 'Vite + React' to 'Test + React'

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Test + React</h1>
       <div className="card">
         <Dialog>
           <DialogTrigger>Open</DialogTrigger>
