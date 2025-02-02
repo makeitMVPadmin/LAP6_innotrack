aebac1d647d51a57f685e3cea67c909d7f663588
Huan Li <huanli01008@gmail.com>
2025-02-02 16:54:33 -0500

    HL create ContentCard component and import card from shadcn

---
+++ src/components/ContentCard.tsx
@@ -0,0 +1,9 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter
+  } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
