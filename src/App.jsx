9441e76d2c305f4a390c30fbdc0b555ca3490699
Nyajal Wal <nyajal.w@gmail.com>
2025-02-03 21:50:24 -0500

    changed ContentCard.jsx back to ContentCard.tsx for now

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import "./App.css";
 import AddBookmark from "./components/AddBookmark";
-import ContentCard from "./components/ContentCard.jsx";
+import ContentCard from "./components/ContentCard";
 
 function App() {
   const [count, setCount] = useState(0);
