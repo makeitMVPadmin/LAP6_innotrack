98d72e601beac3ea143f7f3f0c33493651dec847
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-02-08 02:56:18 -0500

    Add global context for tech area

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 
 import "./App.css";
 import AddBookmark from "./components/AddBookmark";
 import ContentCard from "./components/ContentCard";
 
+export const TechAreaContext = createContext("all");
+
 function App() {
-  const [count, setCount] = useState(0);
+  
+    const [techArea, setTechArea] = useState("all");
 
   return (
-    <>
+    <TechAreaContext.Provider value={[techArea, setTechArea]}>
       <ContentCard></ContentCard>
       <AddBookmark></AddBookmark>
-    </>
+    </TechAreaContext.Provider>
   );
 }
 
