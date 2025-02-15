4f955ee9b0dfdb4c8513d888f973286cfbd07f44
Nyajal Wal <nyajal.w@gmail.com>
2025-02-14 19:15:15 -0500

    updated carousel to display 3 cards at a time

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -16,15 +16,13 @@ const CustomCarousel = () => {
     <Carousel className="w-[100%] max-w-4xl mx-auto">
       <CarouselContent>
         {content.map((item) => (
-          <CarouselItem key={item.id} className="p-4">
+          <CarouselItem key={item.id} className="md:basis-1/2 lg:basis-1/3 p-4">
             <div className="border rounded-lg shadow-md p-4">
               <img
                 src={item.picture}
                 alt={item.title}
                 className="w-full h-48 object-cover rounded-md mt-2"
               />
-              {/* <h3>{item.datepublished}</h3> */}
-              {/* creates error*/}
               <h1 className="text-xl font-semibold">{item.title}</h1>
               <p className="text-gray-600">{item.publisher}</p>
               <p className="text-gray-600">{item.summary}</p>
