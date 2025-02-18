77b86c36b2d1403e0b122da40406bec4786ddec3
Emsnyder94 <76456667+Emsnyder94@users.noreply.github.com>
2025-02-17 22:22:34 -0500

    Fix logo sizing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/Logo.png";
 function Header() {
   return (
     <header>
-      <img src={Logo} alt="Logo" />
+      <img src={Logo} alt="Logo" className="w-logo-width h-logo-auto" />
     </header>
   );
 }
