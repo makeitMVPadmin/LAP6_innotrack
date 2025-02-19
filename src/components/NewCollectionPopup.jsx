cb0eaab63239014bd641392ed8166ffc583a7901
Placid Akat <akat0006@algonquinlive.com>
2025-02-18 21:50:18 -0500

    handles the creation of a new bookmark category name including input validation and error state.

--- src/components/NewCollectionPopup.jsx
+++ src/components/NewCollectionPopup.jsx
@@ -2,44 +2,82 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function NewCollectionPopup() {
     const [newCategoryName, setNewCategoryName] = useState("");
+    const [isNewCollectionPopupOpen, setIsNewCollectionPopupOpen] =
+        useState(false);
+    const [error, setError] = useState(false);
 
     function handleCreateNewCategory() {
         /*
         create a category object
-            field: id, name, userId, contentId, ?createdAt
-        call onCreateCollection
+            fields: id, name, userId, contentId, ?createdAt
+        call onCreateCollection function
             takes a new category object and adds it to the collections array
         reset new cat name
         */
+        const regex = /^[a-zA-Z0-9-_/&+]+$/;
+        newCategoryName.trim();
+        if (regex.test(newCategoryName)) {
+            let newCategory = {
+                id: Date.now(),
+                name: newCategoryName.trim(),
+            };
+            console.log(newCategory);
+            setError(false);
+        } else {
+            setError(true);
+        }
+        setNewCategoryName("");
+    }
+
+    function handleCancel() {
+        setIsNewCollectionPopupOpen(false);
     }
 
     return (
-        <Card className="w-[324px] border-r-2 border-b-2 border-black bg-[#FCFDFD]">
-            <CardContent className="pt-4 px-4">
-                <Input
-                    value={newCategoryName}
-                    onChange={(e) => setNewCategoryName(e.target.value)}
-                    placeholder="Enter collection name"
-                    className="text-lg border-r-2 border-b-2 border-[#182127] rounded-lg"
-                />
-            </CardContent>
-            <CardFooter className="pb-4 px-4 flex justify-between">
-                <Button
-                    onClick={handleCreateNewCategory}
-                    className="bg-yellow-400 hover:bg-yellow-400 text-[#28363F] rounded-lg shadow-customButton focus:outline-none"
-                >
-                    Done
-                </Button>
-                <Button
-                    className="bg-[#FCFDFD] focus:outline-none"
-                    variant="ghost"
-                >
-                    Cancel
-                </Button>
-            </CardFooter>
-        </Card>
+        <>
+            <Button
+                onClick={() => {
+                    setIsNewCollectionPopupOpen(true);
+                    setError(false);
+                }}
+            >
+                Test Add Collection
+            </Button>
+
+            {isNewCollectionPopupOpen && (
+                <Card className="w-[324px] border-black border-r-2 border-b-2  bg-[#FCFDFD]">
+                    <CardContent className="pt-4 px-4">
+                        <Input
+                            value={newCategoryName}
+                            onChange={(e) => setNewCategoryName(e.target.value)}
+                            placeholder="Enter collection name"
+                            className={cn(
+                                "text-lg border-[#182127] border-r-2 border-b-2 rounded-lg",
+                                error && "border-red-600"
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter className="pb-4 px-4 flex justify-between">
+                        <Button
+                            onClick={handleCreateNewCategory}
+                            className="bg-yellow-400 hover:bg-yellow-400 border-black border-r-2 border-b-2 text-[#28363F] rounded-lg shadow-customButton focus:outline-none"
+                        >
+                            Done
+                        </Button>
+                        <Button
+                            onClick={handleCancel}
+                            className="bg-[#FCFDFD] border-black border-r-2 border-b-2 focus:outline-none"
+                            variant="ghost"
+                        >
+                            Cancel
+                        </Button>
+                    </CardFooter>
+                </Card>
+            )}
+        </>
     );
 }
