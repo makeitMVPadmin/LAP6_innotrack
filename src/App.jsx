b6f5c5e3b5e53475c7fa1e0b37e2cb4f21f4b791
Emsnyder94 <76456667+Emsnyder94@users.noreply.github.com>
2025-02-19 19:36:25 -0500

    Fix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import "./App.css";
 import AddBookmark from "./components/AddBookmark";
 // import ContentCard from "./components/ContentCard";
 import TechAreaTab from "./components/TechAreaTab";
-<<<<<<< HEAD
 import Header from "./components/Header";
-=======
 // import ContentList from "./components/ContentList";
 
 import { AppProvider } from "./AppContext";
 import CustomCarousel from "./components/Carousel";
->>>>>>> develop
 
 const hardCodedTechAreas = [
   { id: "all", name: "All" },
@@ -25,24 +22,13 @@ function App() {
   const [selectedTabId, setSelectedTabId] = useState("all");
 
   return (
-<<<<<<< HEAD
-    <>
-      <Header />
-      <TechAreaTab
-        techAreas={hardCodedTechAreas}
-        selectedTabId={selectedTabId}
-        onTabChange={setSelectedTabId}
-      ></TechAreaTab>
-      <ContentCard></ContentCard>
-      <AddBookmark></AddBookmark>
-    </>
-=======
     <AppProvider>
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <Header />
+
         <CustomCarousel />
       </div>
     </AppProvider>
->>>>>>> develop
   );
 }
 
