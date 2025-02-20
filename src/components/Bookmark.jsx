10467cbd6dd9f9250b4d0681d30b581cc25c400b
Placid Akat <akat0006@algonquinlive.com>
2025-02-19 23:55:21 -0500

    installs shadcn's scrollable component

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Bookmark() {
     return (
@@ -14,24 +15,26 @@ export default function Bookmark() {
             <CardHeader className="p-0 mb-4">
                 <CardTitle className="text-2xl">Add to A Collection</CardTitle>
             </CardHeader>
-            <CardContent className="mb-6 pb-0 flex-1">
-                <div className="flex items-center space-x-2 mb-2">
-                    <img
-                        src="../assets/placeholder.svg"
-                        alt=""
-                        className="w-12 h-12 rounded mr-6"
-                    />
-                    <Checkbox
-                        id="123abc"
-                        className="border-[#546672] peer data-[state=checked]:bg-[#0264D4]"
-                    />
-                    <label
-                        htmlFor="123abc"
-                        className="text-lg font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                        Item1
-                    </label>
-                </div>
+            <CardContent className=" flex-1">
+                <ScrollArea className="h-[210px] py-2">
+                    <div className="flex items-center space-x-2 mb-2">
+                        <img
+                            src="../assets/placeholder.svg"
+                            alt=""
+                            className="w-12 h-12 rounded mr-2"
+                        />
+                        <Checkbox
+                            id="123abc"
+                            className="border-[#546672] peer data-[state=checked]:bg-[#0264D4]"
+                        />
+                        <label
+                            htmlFor="123abc"
+                            className="text-lg font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            Item 1
+                        </label>
+                    </div>
+                </ScrollArea>
             </CardContent>
             <CardFooter className="p-0 justify-between">
                 <Button className="bg-[#0264D4] hover:bg-[#0264D4] border-black border-l border-t border-r-2 border-b-2 rounded-lg shadow-customButton">
