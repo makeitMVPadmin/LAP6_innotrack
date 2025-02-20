f0e331431a13b30d3a1f83a12d5123b7e9af0f84
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-02-19 20:44:57 -0500

    Save; edited dialog

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,11 +5,11 @@ import bookmark from "../assets/icons/bookmark.svg";
 export default function Summary({content}){
 
     return(
-        <Dialog>
+        <Dialog className="bg-transparent">
             <DialogTrigger className="bg-transparent border-transparent shadow-none w-auto h-auto p-0">
                 <img src={info} alt="" />
             </DialogTrigger>
-            <DialogContent className="pt-[4.375rem] pb-[2.1875rem] px-[1.5rem]"><DialogHeader>
+            <DialogContent showOverlay={false} className="pt-[4.375rem] pb-[2.1875rem] px-[1.5rem]"><DialogHeader>
                 <DialogTitle className="font-fraunces flex gap-2 text-2xl font-semibold mb-1">Summary <img src={bookmark} alt="" /></DialogTitle>
                 <DialogDescription className="font-montserrat text-xs/4 font-normal text-black">{content}</DialogDescription>
             </DialogHeader>
