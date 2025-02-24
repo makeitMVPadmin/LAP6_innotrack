d80ca5a7e6a1aa72b6a296470e8ec466d581c138
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-02-23 19:23:54 -0500

    Remove App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-
-import "./App.css";
 import AddBookmark from "./components/AddBookmark";
 // import ContentCard from "./components/ContentCard";
 import TechAreaTab from "./components/TechAreaTab";
