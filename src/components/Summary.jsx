ede4f4419bf53633f9a31178f05379763ce28468
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-02-25 04:06:15 -0500

    add tags to summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -7,9 +7,19 @@ export default function Summary() {
     
     const keyPoints = ["Smarter Assistants – AI-driven virtual assistants are becoming more intuitive, personalized, and responsive to user needs.", "Automation & Robotics – Advancements in AI-powered automation are streamlining tasks in homes, workplaces, and industries.", "Immersive AI Experiences – AI is enhancing entertainment, healthcare, and smart devices, creating more seamless and interactive user experiences."];
 
+    const tags = ["DEV", "DATA"];
+
     return (
         <section className="max-w-[103rem] w-full px-[5rem] py-[2rem] mt-[7rem] m-auto bg-[#F6FAFD] rounded-xl border border-black border-solid border-r-2 border-b-2">
-            <h2 className="font-fraunces flex gap-2 text-2xl font-semibold mb-1 tracking-normal">Summary</h2>
+            <h2 className="font-fraunces flex gap-4 items-center text-2xl font-semibold mb-1 tracking-normal">Summary
+                {
+                    tags.map((tag) => {
+                        return (
+                        <span className="px-2.5 py-0.5 text-white rounded-full font-normal bg-[#0264D4] h-fit font-montserrat text-xs">{tag}</span>
+                        );
+                    })
+                }
+            </h2>
             <p className="font-montserrat text-base/[1.5] font-normal text-black mt-[1.25rem]">{content.length && content[0].summary}</p>
             <section className="text-end pt-[1.25rem]">
                 <h2 className="font-fraunces flex gap-2 text-2xl font-semibold mb-1">Key Points</h2>
