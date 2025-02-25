7e0b4947abc4a5ab39d6474fbc976b5a55bd3944
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-02-25 05:20:22 -0500

    change summary on next/previous

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,18 +6,30 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import Summary from "./SummaryDialog";
+import { useEffect, useState } from "react";
 
 const CustomCarousel = () => {
-  const { content } = useAppContext();
+  const { content, setCurrentIndex } = useAppContext();
+
+  const [api, setApi] = useState();
+ 
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+ 
+    api.on("select", () => {
+      setCurrentIndex(api.selectedScrollSnap())
+    })
+  }, [api])
 
   if (content.length === 0) return <p>Loading...</p>;
 
   return (
-    <Carousel className="w-[100%] max-w-4xl mx-auto h-fit pt-[2.25rem]">
+    <Carousel setApi={setApi} className="w-[100%] max-w-4xl mx-auto h-fit pt-[2.25rem]">
       <CarouselContent>
-        {content.map((item) => (
-          <CarouselItem key={item.id} className="basis:1/1 p-4">
+        {content.map((item, index) => (
+          <CarouselItem key={item.id} index={index} className="basis:1/1 p-4">
             <div className="border rounded-lg shadow-md p-4">
               <img
                 src={item.picture}
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,9 +1,8 @@
 import { useAppContext } from "@/AppContext";
-import { Button } from "./ui/button";
 
 export default function Summary() {
 
-    const { content } = useAppContext();
+    const { content, currentIndex } = useAppContext();
     
     const keyPoints = ["Smarter Assistants – AI-driven virtual assistants are becoming more intuitive, personalized, and responsive to user needs.", "Automation & Robotics – Advancements in AI-powered automation are streamlining tasks in homes, workplaces, and industries.", "Immersive AI Experiences – AI is enhancing entertainment, healthcare, and smart devices, creating more seamless and interactive user experiences."];
 
@@ -13,14 +12,14 @@ export default function Summary() {
         <section className="max-w-[103rem] w-full px-[5rem] py-[2rem] mt-[7rem] m-auto bg-[#F6FAFD] rounded-xl border border-black border-solid border-r-2 border-b-2">
             <h2 className="font-fraunces flex gap-4 items-center text-2xl font-semibold mb-1 tracking-normal">Summary
                 {
-                    tags.map((tag) => {
+                    tags.map((tag, index) => {
                         return (
-                        <span className="px-2.5 py-0.5 text-white rounded-full font-normal bg-[#0264D4] h-fit font-montserrat text-xs">{tag}</span>
+                        <span key={index} className="px-2.5 py-0.5 text-white rounded-full font-normal bg-[#0264D4] h-fit font-montserrat text-xs">{tag}</span>
                         );
                     })
                 }
             </h2>
-            <p className="font-montserrat text-base/[1.5] font-normal text-black mt-[1.25rem]">{content.length && content[0].summary}</p>
+            <p className="font-montserrat text-base/[1.5] font-normal text-black mt-[1.25rem]">{content.length && content[currentIndex].summary}</p>
             <section className="text-end pt-[1.25rem]">
                 <h2 className="font-fraunces flex gap-2 text-2xl font-semibold mb-1">Key Points</h2>
                 <ul className="mt-3">
