dbcbf9c8ff64580aec9b97db7e4c516e67845669
Nyajal Wal <nyajal.w@gmail.com>
2025-02-24 19:03:30 -0500

    added avatar and menubar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import Logo from "../assets/Logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 function Header() {
   return (
     <header>
       <img src={Logo} alt="Logo" className="w-logo-width h-logo-auto" />
+      <div>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>Innotrack</MenubarTrigger>
+            <MenubarTrigger>Hot Topics</MenubarTrigger>
+            <MenubarTrigger>Home</MenubarTrigger>
+          </MenubarMenu>
+        </Menubar>
+
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
     </header>
   );
 }
