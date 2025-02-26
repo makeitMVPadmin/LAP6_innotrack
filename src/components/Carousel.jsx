a7e485968d527683e1d556b24c36078bb2ae651d
Nyajal Wal <nyajal.w@gmail.com>
2025-02-26 17:50:29 -0500

    styling carousel item

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -28,20 +28,20 @@ const CustomCarousel = () => {
   }, [api]);
 
   // **Fix: Index not updating when clicking next**
-  useEffect(() => {
-    if (!api) return;
+  // useEffect(() => {
+  //   if (!api) return;
 
-    const onSelect = () => {
-      setCurrentIndex(api.selectedScrollSnap() + 1);
-    };
+  //   const onSelect = () => {
+  //     setCurrentIndex(api.selectedScrollSnap() + 1);
+  //   };
 
-    api.on("select", onSelect);
-    onSelect();
+  //   api.on("select", onSelect);
+  //   onSelect();
 
-    return () => {
-      api.off("select", onSelect);
-    };
-  }, [api]);
+  //   return () => {
+  //     api.off("select", onSelect);
+  //   };
+  // }, [api]);
 
   if (content.length === 0) return <p>Loading...</p>;
 
@@ -100,18 +100,33 @@ const CustomCarousel = () => {
       <CarouselContent>
         {content.map((item, index) => (
           <CarouselItem key={item.id} index={index} className="basis:1/1 p-4">
-            <div className="border rounded-lg shadow-md p-4">
+            <div
+              className="rounded-[7.869px]
+  //           border-t-[0.997px] border-r-[1.994px] border-b-[1.994px] border-l-[0.997px] border-[#182127]
+  //           shadow-[0px_0.997px_1.994px_0px_rgba(0,0,0,0.05)]
+  //           bg-white overflow-hidden "
+            >
               <img
                 src={item.picture}
                 alt={item.title}
-                className="w-full h-48 object-cover rounded-md mt-2"
+                className="w-full aspect-[56/25] object-cover rounded-t-[7.869px]"
               />
-              <div className="flex justify-between">
-                <h1 className="text-xl font-semibold">{item.title}</h1>
-                <Bookmark contentInfo={item.id} />
+              <div className="p-2  bg-[#EBF1F6]">
+                <div className="flex justify-between">
+                  <h1 className="text-xl font-semibold">{item.title}</h1>
+                  <Bookmark contentInfo={item.id} />
+                </div>
+
+                <div className="flex justify-between">
+                  <div>
+                    <p className="text-sm sm:text-base">{item.publisher}</p>
+                    {/* <p className="text-gray-600">{item.summary}</p> */}
+                  </div>
+                  <button className="mt-2 sm:mt-4 bg-yellow-400 text-black font-medium px-4 py-2 rounded-md shadow-md hover:bg-yellow-500 transition">
+                    Read More
+                  </button>
+                </div>
               </div>
-              <p className="text-gray-600">{item.publisher}</p>
-              <p className="text-gray-600">{item.summary}</p>
             </div>
           </CarouselItem>
         ))}
