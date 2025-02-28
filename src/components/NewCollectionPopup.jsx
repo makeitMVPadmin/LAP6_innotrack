e28c133fef9dbe37b73833a988bbe35aa6bb6d7c
Placid Akat <akat0006@algonquinlive.com>
2025-02-27 20:36:58 -0500

    fixes bug: bookmark icon change is now working in summary component

--- src/components/NewCollectionPopup.jsx
+++ src/components/NewCollectionPopup.jsx
@@ -78,10 +78,7 @@ export default function NewCollectionPopup({ onCreateCollection, bookmarks }) {
                         New Collection
                     </Button>
                 </DialogTrigger>
-                <DialogContent
-                    OnInteractOutside={(e) => e.preventDefault()}
-                    className="p-0 bg-transparent border-none shadow-none top-3/4"
-                >
+                <DialogContent className="p-0 bg-transparent border-none shadow-none top-3/4">
                     <div
                         className={cn(
                             " w-full max-w-md transform transition-all duration-300 ease-in-out",
